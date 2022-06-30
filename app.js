57aecb589cc253d5edba35d02d21eab55671c0ff
LUIS I ROSALES JR <luis.rosalesjr01@gmail.com>
2022-06-29 22:06:09 -0600

    needed console

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
-prompt("what is your favorite workout"); 
+let usersWorkout = prompt("what is your favorite workout"); 
+if (usersWorkout == "bench") {
+    console.log("STRENGTH IS FOR YOU!");
+}
+else if (usersWorkout == "BENCH"){
+    console.log("this site is for you");
+}
+if (usersWorkout == "BENCH"){
+    document.write("THIS SITE IS FOR YOU!" );
+}
+else if (usersWorkout.toLowerCase() == "bench"){
+    document.write("try strength");
+}
+else{
+    document.write("strength still might be for you");
+}
+
+
+
 
